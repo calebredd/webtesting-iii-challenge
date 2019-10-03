74e95bc86a21bd29accd14e85fbc67223077c2f0
Caleb Redd <caleb.redd@yahoo.com>
2019-10-02 21:26:20 -0700

    Added tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,33 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test("<Controls/>", () => {
+  const tree = render(<Controls />);
+
+  expect(tree.baseElement).toMatchSnapshot();
+});
+
+test("should render correctly", () => {
+  const locked = false;
+  const closed = true;
+  const toggleLockedMock = jest.fn();
+  const toggleClosedMock = jest.fn();
+  const { getByText } = render(
+    <Controls
+      locked={locked}
+      closed={closed}
+      toggleLocked={toggleLockedMock}
+      toggleClosed={toggleClosedMock}
+    />
+  );
+
+  const lockButton = getByText(/open gate/i);
+  const closeButton = getByText(/lock gate/i);
+  fireEvent.click(closeButton);
+  fireEvent.click(lockButton);
+  expect(toggleLockedMock).toHaveBeenCalled();
+  expect(toggleClosedMock).toHaveBeenCalled();
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
 // Test away
+import React from "react";
+// import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+test("<Dashboard/>", () => {
+  const tree = render(<Dashboard />);
+
+  expect(tree.baseElement).toMatchSnapshot();
+});
+
+// test("it is mocking me", () => {
+//   const mock = jest.fn();
+//   // const actual = mock("smile");
+
+//   expect(mock).toHaveBeenCalled();
+//   // expect(mock).toHaveBeenCalledTimes(2);
+//   expect(mock).toHaveBeenCalledTimes(1);
+//   expect(mock).toHaveBeenCalledWith("smile");
+//   // expect(mock).toHaveBeenCalledWith("we");
+// });
